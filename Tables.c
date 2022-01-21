b4f7edf66a716c95dc0efa16f0594763a4754f2c
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-01-21 17:44:33 +0530

    #include<stdio.h> int main() {     int a,b,i;     scanf("%d%d",&a,&b);     for(i=1;i<=b;i++)     {         if(i%2==0)         {             continue;         }         printf("%d x %d = %d ",a,i,a*i);     }     return 0; }

---
+++ Tables.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=b;i++)
+    {
+        if(i%2==0)
+        {
+            continue;
+        }
+        printf("%d x %d = %d
+",a,i,a*i);
+    }
+    return 0;
+}
