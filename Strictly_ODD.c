7f67122d2fef8d85196fadd748a49f48b6d8f15f
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-02-11 23:22:04 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,c=0,so=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         if(a[i]%2!=0)         {             c++;         }         if(a[i]%2!=0 && i%2!=0)         {             so++;         }     }     if(c==so)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,c=0,so=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]%2!=0)
+        {
+            c++;
+        }
+        if(a[i]%2!=0 && i%2!=0)
+        {
+            so++;
+        }
+    }
+    if(c==so)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
