9dbf5a19ab43b2891fa6f58e19c2c157e083f02f
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-02-12 18:50:17 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,j,d,m=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n-1;i++)     {         for(j=i+1;j<=n-1;j++)         {             d=a[j]-a[i];             m=d>m?d:m;         }     }     printf("%d",m); }

---
+++ Best_Time_to_Buy_and_Sell_Stock.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,j,d,m=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);  
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<=n-1;j++)
+        {
+            d=a[j]-a[i];
+            m=d>m?d:m; 
+        }
+    }
+    printf("%d",m);
+}
