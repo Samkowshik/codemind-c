cef63c37b01b88db0c5c62e65e5e16b4c5789802
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-02-18 21:27:08 +0530

    #include<stdio.h> int main() {     int n,i,s,s1=0,s2=1;     scanf("%d",&n);     printf("0 1 ");     for(i=0;i<n-2;i++)     {         s=s1+s2;         printf("%d ",s);         s1=s2;         s2=s;     } }

---
+++ Play_with_Fibonacci_sequence.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,s,s1=0,s2=1;
+    scanf("%d",&n);
+    printf("0 1 ");
+    for(i=0;i<n-2;i++)
+    {
+        s=s1+s2;
+        printf("%d ",s);
+        s1=s2;
+        s2=s;
+    }
+}
