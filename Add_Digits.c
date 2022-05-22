0400c2081605e1dc45428baa41a64d386dfa4758
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-05-22 11:11:03 +0530

    #include<stdio.h> int main() {     int a,t,r;     scanf("%d",&a);     while(a/10!=0)     {         t=a;         a=0;         while(t)         {             r=t%10;             a=a+r;             t=t/10;         }     }    printf("%d",a); }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,s;
-    scanf("%d",&n);
-    while(n/10!=0)
+    int a,t,r;
+    scanf("%d",&a);
+    while(a/10!=0)
     {
-        s=0;
-        while(n!=0)
+        t=a;
+        a=0;
+        while(t)
         {
-            r=n%10;
-            s=s+r;
-            n=n/10;
+            r=t%10;
+            a=a+r;
+            t=t/10;
         }
-        n=s;
     }
-    printf("%d",s);
+   printf("%d",a);
 }
