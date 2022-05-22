ac1767115d2dd908ff16728578f772ee7a0e77d9
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-05-22 15:13:29 +0530

    #include<stdio.h> int main() {     int a,b,i;     scanf("%d%d",&a,&b);     for(i=a;i<=b;i--)     {         if(a%i==0&&b%i==0)         {             printf("%d",i);             break;         }     } }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,i,m;
+    int a,b,i;
     scanf("%d%d",&a,&b);
-    m=a>b?a:b;
-    for(i=m/2;i>=1;i--)
+    for(i=a;i<=b;i--)
     {
         if(a%i==0&&b%i==0)
         {
