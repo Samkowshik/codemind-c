1218e856006de3b9cadb0b9173f39a599502950a
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-05-22 11:53:53 +0530

    #include<stdio.h> int main() {     int n,o=1,p=0,s,r,x;     scanf("%d",&n);     x=n;     while(x)     {         r=x%10;         if(r==6)         {             p=o;         }         o=o*10;         x=x/10;     }     printf("%d",n+3*p); }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,o=1,p=0,x;
+    int n,o=1,p=0,s,r,x;
     scanf("%d",&n);
     x=n;
-    while(x!=0)
+    while(x)
     {
         r=x%10;
         if(r==6)
         {
-        	p=o;
-		}
-		o=o*10;
+            p=o;
+        }
+        o=o*10;
         x=x/10;
     }
     printf("%d",n+3*p);
-    return 0;
 }
