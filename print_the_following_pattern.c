b51d46ac0bcd2578669aee44dd0eb6a5bf634f88
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-11-05 23:55:31 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%c ",64+i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,20 +3,13 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    for(i=1;i<n;i++)
+    for(i=1;i<=n;i++)
     {
         for(j=1;j<=n;j++)
         {
-            if(j==1 ||i==j)
-            printf("*");
-            else
-            printf(" ");
+            printf("%c ",64+i);
         }
         printf("
 ");
-    }
-    for(i=1;i<=n;i++)
-    {
-        printf("*");
     }
 }
